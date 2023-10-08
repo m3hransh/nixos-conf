e677b392c5a979da16dde7f904a43033f56bf05a
Mehran Shahidi <m.mehrashahidi@gmail.com>
2023-10-08 21:29:02 +0200

    feat: add script grimblast

--- home/home.nix
+++ home/home.nix
@@ -5,6 +5,7 @@
   imports = [
     hyprland.homeManagerModules.default
     ./programs
+    ./scripts
   ];
 
   home = {
@@ -86,5 +87,48 @@
       # Enable a plugin (here grc for colorized command output)
       { name = "grc"; src = pkgs.fishPlugins.grc.src; }
     ];
+  };
+
+  # GTK
+
+  home.sessionVariables = {
+    GTK_THEME = "Nordic";
+  };
+  home.pointerCursor = {
+    package = pkgs.catppuccin-cursors;
+    name = "Catppuccin-Frappe-Dark";
+    size = 16;
+  };
+  home.pointerCursor.gtk.enable = true;
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Nordic";
+      package = pkgs.nordic;
+    };
+    cursorTheme = {
+      name = "Catppuccin-Frappe-Dark";
+    };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      size = 12;
+    };
+    gtk3.extraConfig = {
+      gtk-xft-antialias = 1;
+      gtk-xft-hinting = 1;
+      gtk-xft-hintstyle = "hintslight";
+      gtk-xft-rgba = "rgb";
+    };
+    gtk2.extraConfig = ''
+      gtk-xft-antialias=1
+      gtk-xft-hinting=1
+      gtk-xft-hintstyle="hintslight"
+      gtk-xft-rgba="rgb"
+    '';
   };
 }
--- home/programs/waybar/custom_modules/memory.sh
+++ home/programs/waybar/custom_modules/memory.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env Bash
+#!/usr/bin/env bash
 
 text=$(free -h | grep Mem | awk '{print $3}' | sed -e 's/\(\d*[MG]\).*/\1/')
 tooltip="$text used"
