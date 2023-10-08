711a36fb563274305ff8068d298763facd600773
Mehran Shahidi <m.mehrashahidi@gmail.com>
2023-10-08 20:37:00 +0200

    feat: vscode conf update

--- home/programs/vscode/default.nix
+++ home/programs/vscode/default.nix
@@ -4,6 +4,7 @@
     # home.file.".config/Code/User/settings.json".source= ./settings.json;
     programs.vscode = {
       enable = true;
+      package = pkgs.vscode.fhs;
       userSettings = builtins.fromJSON (builtins.readFile ./settings.json );
   };
 }
