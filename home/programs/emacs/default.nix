7885ea39fe09a3b3dfc712b985434ee8f0a8bd74
Mehran Shahidi <m.mehrashahidi@gmail.com>
2025-06-26 11:46:23 +0200

    📦update emacs

--- home/programs/emacs/default.nix
+++ home/programs/emacs/default.nix
@@ -3,7 +3,7 @@
 {
   programs.emacs = {
     enable = true;
-    package = pkgs.emacs29-pgtk; # replace with pkgs.emacs-gtk, or a version provided by the community overlay if desired.
+    package = pkgs.emacs-pgtk; # replace with pkgs.emacs-gtk, or a version provided by the community overlay if desired.
     extraConfig = builtins.readFile ./init.el;
     extraPackages = epkgs:
       (with epkgs; [
