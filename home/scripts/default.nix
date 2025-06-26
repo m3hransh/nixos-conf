581eb776c902fde135f35750ee5c80bccd23cec7
Mehran Shahidi <m.mehrashahidi@gmail.com>
2025-06-26 11:46:44 +0200

    ✨add nvidia offload script

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -36,10 +36,19 @@ let
       "$NIX_DIR#nixosConfigurations.$CONFIG_PATH" \
       || { echo "Failed to evaluate path: $CONFIG_PATH"; exit 1; }
   '';
+  # Add this to your Home Manager or system configuration
+  nvidia-offload =  pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec -a "$0" "$@"
+  '';
 in
 {
   home.packages = with pkgs; [
     grimblast_watermark
     nix_opts
+    nvidia-offload
   ];
 }
